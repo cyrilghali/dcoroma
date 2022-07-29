8cf4f94d2a174e8a4bad58d5b48257ff40b60e01
Cyril <cyril.ghali@hotmail.com>
2022-07-29 16:37:21 +0200

    Tailwind CSS init + header added

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Header from '../components/Header'
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
