c57cc81a17550c84438bf476156906e4bbd71786
Cyril <cyril.ghali@hotmail.com>
2022-07-31 21:03:16 +0200

    agpia.pdf

--- pages/library/liturgy.tsx
+++ pages/library/liturgy.tsx
@@ -5,7 +5,7 @@ import { Title } from '../../components/Title'
 import LibraryTables from '../../components/LibraryTable'
 
 const documents = [
-  {name: 'Agpeya', description: 'this is the book of our prayers', href: '/files/agpeya.pdf'},
+  {name: 'Agpeya', description: 'this is the book of our prayers', href: '/files/agpia.pdf'},
   {name: 'Il Santo Messale', description: 'this is the book of our prayers', href: '/files/Il_santo_messale.pdf'},
   {name: 'Santo Messale Grande', description: 'this is the book of our prayers', href: '/files/santo_messale_grande.pdf'},
   {name: 'Messa Pasqua 2020', description: 'this is the book of our prayers', href: '/files/pasqua_2020.pdf'},
