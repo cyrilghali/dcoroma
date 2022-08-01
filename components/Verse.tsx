457efbba8662d169476146192afd6f4ef95d1e04
Cyril <cyril.ghali@hotmail.com>
2022-08-01 19:30:43 +0200

    donaztion wip

--- components/Verse.tsx
+++ components/Verse.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Verse() {
   return (
     <div className="bg-white pt-16 lg:py-24">
@@ -7,9 +9,10 @@ export default function Verse() {
             <div aria-hidden="true" className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
             <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
               <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-                <img
-                  className="object-cover lg:h-full lg:w-full"
-                  src="https://cdn.britannica.com/82/130682-050-21F7C58B/Coptic-Orthodox-Church-Amman-Jordan.jpg"
+                <Image
+                  className=" aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full object-cover lg:h-full lg:w-full"
+                  src="/photos/church.jpg"
+                  layout="fill"
                   alt=""
                 />
               </div>
