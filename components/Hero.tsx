d6eb61a912cd7132ad864d07c8e7e59c2649c0aa
Cyril <cyril.ghali@hotmail.com>
2022-08-04 09:31:53 +0200

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,44 +1,6 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-
-import {
-  LocationMarkerIcon,
-  MenuIcon,
-  HeartIcon,
-  SupportIcon,
-  HomeIcon,
-  XIcon,
-  ExternalLinkIcon,
-  NewspaperIcon,
-  BookOpenIcon,
-} from '@heroicons/react/outline'
 import Image from 'next/image'
+import Header from './Header'
 
-const navigation = [
-  {
-    name: 'Home',
-    href: '/',
-    icon: HomeIcon,
-  },
-  {
-    name: 'Chiese',
-    href: '/churches',
-    icon: LocationMarkerIcon,
-  },
-  { name: 'Fare una donazione', href: '/donation', icon: HeartIcon},
-  { name: 'Katameros', href: 'https://katameros.app/?lang=it', icon: ExternalLinkIcon },
-  { name: 'Notizie', href: '/news', icon: NewspaperIcon },
-  {
-    name: 'Libreria',
-    href: '/library',
-    icon: BookOpenIcon,
-  },
-  {
-    name: 'Contatti',
-    href: '/contact',
-    icon: SupportIcon,
-  },
-]
 export default function Hero() {
   return (
     <div className="relative bg-white overflow-hidden">
@@ -66,80 +28,8 @@ export default function Hero() {
           <rect x={118} width={404} height={784} fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)" />
         </svg>
       </div>
-
-      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
-        <Popover>
-          <nav
-            className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-            aria-label="Global"
-          >
-            <div className="flex items-center flex-1">
-              <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="" className="h-10 w-auto">
-                  <span className="sr-only">Workflow</span>
-                  <Image width={50} height={50} src={'/logo1.png'} alt={'logo'} />
-                </a>
-                <div className="mr-2 flex items-center md:hidden">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-800">
-                    <span className="sr-only">Open main menu</span>
-                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-              <div className="hidden md:block md:ml-10 md:space-x-10">
-                {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </nav>
-
-          <Transition
-            as={Fragment}
-            enter="duration-150 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-100 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
-              focus
-              className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-            >
-              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <a href="" className="h-10 w-auto">
-                      <Image width={50} height={50} src={'/logo.png'} alt={'logo'}/>
-                    </a>
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-800">
-                      <span className="sr-only">Close main menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                <div className="px-2 pt-2 pb-3 space-y-1">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                    >
-                      <item.icon className="flex-shrink-0 h-6 w-6 text-rose-900" aria-hidden="true" />
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </Popover>
-
+      <Header />
+      <div className="relative pb-16 sm:pb-24 lg:pb-32">
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
