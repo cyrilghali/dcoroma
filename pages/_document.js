16a9d22590a6523eef4087fa831cbc7fba178e0c
Cyril <cyril.ghali@hotmail.com>
2022-08-06 08:05:30 +0200

    analytics:

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <script
+        async
+        id="chiffre:analytics"
+        src="https://chiffre.io/analytics.js"
+        data-chiffre-project-id="Gp2eDpPv6kjk4swo"
+        data-chiffre-public-key="pk.T4QI36BEBNT8_O6JN4mNQiANxQ381FAtNtJ6ItcM2HE"
+        referrerpolicy="origin"
+        crossOrigin="anonymous"
+      />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
