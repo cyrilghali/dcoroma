d5b87f44fa4afa8c86651292eda69a8ddb023c97
Cyril <cyril.ghali@hotmail.com>
2022-08-08 07:42:10 +0200

    correct card and remove useless code

---
+++ pages/library/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Header from '../../components/Header'
+import LibraryCards from '../../components/LibraryCards'
+import Footer from '../../components/Footer'
+import { Title } from '../../components/Title'
+
+const Library: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <Title title="Libreria" /> 
+      <LibraryCards /> 
+      <Footer />
+    </div>
+  )
+}
+
+export default Library
