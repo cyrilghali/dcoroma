f265c12acaa19716b5399955d5066d8157faba99
Cyril <cyril.ghali@hotmail.com>
2022-08-13 00:02:47 +0200

    ok

--- data/churches/genova.js
+++ data/churches/genova.js
@@ -6,9 +6,9 @@ const genova = [
   churchName: "Chiesa di San Antonio",
   churchImageUrl: "",
   referentID: 11,
-  monthlyMassSchedule: 'Non disponibile',
-  location: '',
-  locationUrl: ''
+  monthlyMassSchedule: 'La Messa viene celebrata la prima, la seconda e la terza domenica del mese, il quarto sabato del mese e tutti i lunedì dalle 9 alle 11.',
+  location: 'Passo della Zebra, 6C, 16143 Genova GE',
+  locationUrl: 'https://goo.gl/maps/q1gvt7jALzWPeBdg6'
 },
 {
   name: "San Giorgio",
@@ -17,8 +17,8 @@ const genova = [
   churchName: "Chiesa di San Giorgio",
   churchImageUrl: "",
   referentID: 11,
-  monthlyMassSchedule: 'Non disponibile',
-  location: '',
+  monthlyMassSchedule: 'La Messa viene celebrata la terza e la quarta domenica del mese e il primo e il secondo sabato del mese, dalle 9.00 alle 11.00.',
+  location: 'Piazza J.F Kennedy 74 ',
   locationUrl: ''
 },
 ]
