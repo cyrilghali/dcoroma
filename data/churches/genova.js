c78760b79f7d75fb4692459fbb1321e6a25b7f19
Cyril <cyril.ghali@hotmail.com>
2022-08-13 08:20:21 +0200

    genova

--- data/churches/genova.js
+++ data/churches/genova.js
@@ -1,10 +1,10 @@
 const genova = [
 {
   name: "San Antonio",
-  imageUrl:"",
+  imageUrl:"/photos/anbaantonios.jpeg",
   slug: 'sanantonio',
   churchName: "Chiesa di San Antonio",
-  churchImageUrl: "",
+  churchImageUrl: "/photos/genova-sanantonio.jpg",
   referentID: 11,
   monthlyMassSchedule: 'La Messa viene celebrata la prima, la seconda e la terza domenica del mese, il quarto sabato del mese e tutti i lunedì dalle 9 alle 11.',
   location: 'Passo della Zebra, 6C, 16143 Genova GE',
@@ -15,11 +15,11 @@ const genova = [
   imageUrl:"/photos/sangiorgio.jpg",
   slug: 'sangiorgio',
   churchName: "Chiesa di San Giorgio",
-  churchImageUrl: "",
+  churchImageUrl: "/photos/genova-laspezia.jpg",
   referentID: 11,
   monthlyMassSchedule: 'La Messa viene celebrata la terza e la quarta domenica del mese e il primo e il secondo sabato del mese, dalle 9.00 alle 11.00.',
   location: 'Piazza J.F Kennedy 74 ',
-  locationUrl: ''
+  locationUrl: 'https://goo.gl/maps/jiaXUpctXym75QGB6'
 },
 ]
 export default genova
