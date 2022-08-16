0409b48d6d1c129f7c29da42737a95b17705bf89
Cyril <cyril.ghali@hotmail.com>
2022-08-16 15:00:03 +0200

    data

--- data/library/liturgy.js
+++ data/library/liturgy.js
@@ -2,19 +2,19 @@ const liturgy = [
   {name: 'Agpeya', description: '', href: '/files/agpia.pdf'},
   {name: 'Il Santo Messale', description: '', href: '/files/Il_santo_messale.pdf'},
   {name: 'Santo Messale Grande', description: '', href: '/files/santo_messale_grande.pdf'},
-  {name: 'Messa Pasqua 2020', description: '', href: '/files/pasqua_2020.pdf'},
+  {name: 'Messa Pasqua', description: '', href: '/files/pasqua_2020.pdf'},
   {name: 'Katameros Domeniche', description: '', href: '/files/katameros_domeniche.pdf'},
   {name: 'Rito dei funerali', description: '', href: '/files/ritodeifunerali.pdf'},
   {name: 'Fidanzamento-Matrimonio', description: '', href: '/files/Fidanzamento-Matrimonio.pdf'},
-  {name: 'Lezionario', description: '', href: '/files/lezionario.pdf'},
+  {name: 'Lezionario della Santa Cinquantina di Pasqua', description: '', href: '/files/lezionario.pdf'},
   {name: 'Lessionario annuale', description: '', href: '/files/lessionario_annuale.pdf'},
   {name: 'Il Sinassario', description: '', href: '/files/synexar.pdf'},
   {name: 'Tamgid', description:'', href:'/files/tamgid.pdf'},
   {name: 'Corso Matrimonio', description: '', href: '/files/CORSO_MATRIMONIO.pdf'},
   {name: 'Rito del Battesimo', description: '', href: '/files/ritodelbattesimo.pdf'},
-  {name: 'Rito della Lampada - 2018', description: '', href: '/files/Rito_della_lampada_2018.pdf'},
+  {name: 'Rito della Lampada', description: '', href: '/files/Rito_della_lampada_2018.pdf'},
   {name: 'Benedizione della Casa Nuova', description: '', href: '/files/benedizione_della_casa_nuova.pdf'},
-  {name: 'Rito della settimana Santa - 2018', description: '', href: '/files/Rito_della_settimana_Santa_2018.pdf'},
+  {name: 'Rito della settimana Santa', description: '', href: '/files/settimanasanta.pdf'},
   // More documents...
 ]
 
