6cbf55097d71f1aef2adcce132031c96bff56282
Cyril <cyril.ghali@hotmail.com>
2022-08-16 15:07:54 +0200

    data

--- data/library/tasbeha.js
+++ data/library/tasbeha.js
@@ -1,5 +1,6 @@
 const tasbeha = [
   {name: 'Tasbeha Annuale', description: '', href: '/files/Tasbeha Annuale.pdf'},
+  {name: 'Tasbeha Kiahk', description: '', href: '/files/tasbeha_kiahk.pdf'},
   // More documents...
 ]
 
