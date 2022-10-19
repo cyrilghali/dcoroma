1ab5f16bd03ad253939ec1a822111e19d4156598
Cyril <cyril.ghali@hotmail.com>
2022-10-19 13:07:50 +0200

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,8 @@ export default function Document() {
       <Head />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet" /> 
-      <script
-        async
-        id="chiffre:analytics"
-        src="https://chiffre.io/analytics.js"
-        data-chiffre-project-id="Gp2eDpPv6kjk4swo"
-        data-chiffre-public-key="pk.T4QI36BEBNT8_O6JN4mNQiANxQ381FAtNtJ6ItcM2HE"
-        referrerPolicy="origin"
-        crossOrigin="anonymous"
-      />
+      <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet" />
+      <script async defer data-website-id="489d0adc-80b6-48e8-af24-0c669c240565" src="https://umami.cyrilghali.fr/umami.js"></script>
       <body>
         <Main />
         <NextScript />
