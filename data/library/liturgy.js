4cb50e4fca00d78f33b0b360014f96a15259d3cc
Cyril Ghali <cyril@cyrilghali.fr>
2023-01-19 11:04:09 +0100

    remove paypal button + add doc nativity

--- data/library/liturgy.js
+++ data/library/liturgy.js
@@ -1,22 +1,75 @@
 const liturgy = [
-  {name: 'Agpeya', description: '', href: '/files/Agbia dato a Gamil.pdf'},
-  {name: 'Il Santo Messale', description: '', href: '/files/Il_santo_messale.pdf'},
-  {name: 'Santo Messale Grande', description: '', href: '/files/santo_messale_grande.pdf'},
-  {name: 'Messa Pasqua', description: '', href: '/files/pasqua_2020.pdf'},
-  {name: 'Lezionario Annuale Domeniche', description: '', href: '/files/katameros_domeniche.pdf'},
-  {name: 'Rito dei funerali', description: '', href: '/files/ritodeifunerali.pdf'},
-  {name: 'Fidanzamento-Matrimonio', description: '', href: '/files/Fidanzamento-Matrimonio.pdf'},
-  {name: 'Lezionario della Santa Cinquantina di Pasqua', description: '', href: '/files/lezionario.pdf'},
-  {name: 'Lezionario annuale feriale', description: '', href: '/files/lessionario_annuale.pdf'},
-  {name: 'Lezionario Quaresima', description: '', href: '/files/lezionario_quaresima.pdf'},
-  {name: 'Il Sinassario', description: '', href: '/files/synexar.pdf'},
-  {name: 'Tamgid', description:'', href:'/files/tamgid.pdf'},
-  {name: 'Corso Matrimonio', description: '', href: '/files/CORSO_MATRIMONIO.pdf'},
-  {name: 'Rito del Battesimo', description: '', href: '/files/ritodelbattesimo.pdf'},
-  {name: 'Rito della Lampada', description: '', href: '/files/Rito_della_lampada_2018.pdf'},
-  {name: 'Benedizione della Casa Nuova', description: '', href: '/files/benedizione_della_casa_nuova.pdf'},
-  {name: 'Rito della settimana Santa', description: '', href: '/files/settimanasanta.pdf'},
+  { name: 'Agpeya', description: '', href: '/files/Agbia dato a Gamil.pdf' },
+  {
+    name: 'Il Santo Messale',
+    description: '',
+    href: '/files/Il_santo_messale.pdf',
+  },
+  {
+    name: 'Santo Messale Grande',
+    description: '',
+    href: '/files/santo_messale_grande.pdf',
+  },
+  { name: 'Messa Pasqua', description: '', href: '/files/pasqua_2020.pdf' },
+  {
+    name: 'Lezionario Annuale Domeniche',
+    description: '',
+    href: '/files/katameros_domeniche.pdf',
+  },
+  {
+    name: 'Rito dei funerali',
+    description: '',
+    href: '/files/ritodeifunerali.pdf',
+  },
+  {
+    name: 'Fidanzamento-Matrimonio',
+    description: '',
+    href: '/files/Fidanzamento-Matrimonio.pdf',
+  },
+  {
+    name: 'Lezionario della Santa Cinquantina di Pasqua',
+    description: '',
+    href: '/files/lezionario.pdf',
+  },
+  {
+    name: 'Lezionario annuale feriale',
+    description: '',
+    href: '/files/lessionario_annuale.pdf',
+  },
+  {
+    name: 'Lezionario Quaresima',
+    description: '',
+    href: '/files/lezionario_quaresima.pdf',
+  },
+  { name: 'Il Sinassario', description: '', href: '/files/synexar.pdf' },
+  { name: 'Tamgid', description: '', href: '/files/tamgid.pdf' },
+  {
+    name: 'Corso Matrimonio',
+    description: '',
+    href: '/files/CORSO_MATRIMONIO.pdf',
+  },
+  {
+    name: 'Rito del Battesimo',
+    description: '',
+    href: '/files/ritodelbattesimo.pdf',
+  },
+  {
+    name: 'Rito della Lampada',
+    description: '',
+    href: '/files/Rito_della_lampada_2018.pdf',
+  },
+  {
+    name: 'Benedizione della Casa Nuova',
+    description: '',
+    href: '/files/benedizione_della_casa_nuova.pdf',
+  },
+  { name: 'Nativity', description: '', href: '/files/nativity.pdf' },
+  {
+    name: 'Rito della settimana Santa',
+    description: '',
+    href: '/files/settimanasanta.pdf',
+  },
   // More documents...
-]
+];
 
-export default liturgy
+export default liturgy;
--- pages/donation.tsx
+++ pages/donation.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import { Title } from '../components/Title'
-import Verse from '../components/Verse'
-import Divider from '../components/Divider'
-import Paypal from '../components/Paypal'
-import IBAN from '../components/IBAN'
+import type { NextPage } from 'next';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Title } from '../components/Title';
+import Verse from '../components/Verse';
+import Divider from '../components/Divider';
+import IBAN from '../components/IBAN';
 
 const Contact: NextPage = () => {
   return (
@@ -14,12 +13,10 @@ const Contact: NextPage = () => {
       <Title title="Donazione" />
       <Verse />
       <Divider />
-      <Paypal />
-      <Divider />
       <IBAN />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
