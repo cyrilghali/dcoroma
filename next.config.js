6d2012c740cfa81bec9a8abf9ae43c7ab3a7598a
Cyril Ghali <contact@cyrilghali.fr>
2023-09-11 16:11:31 +0200

    cloudflare_pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['eglisecopte.ch', 'strapi.dcoroma.it'],
-  },
-  swcMinify: true,
+  output: 'standalone',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
+ 
 module.exports = nextConfig
