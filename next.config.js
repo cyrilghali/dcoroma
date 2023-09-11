e6a5c3cb703d7267f9066eb5f9901e843df7e6de
Cyril GHALI <contact@cyrilghali.fr>
2023-09-11 22:05:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
   images: {
     domains: ['eglisecopte.ch', 'strapi.dcoroma.it'],
   },
