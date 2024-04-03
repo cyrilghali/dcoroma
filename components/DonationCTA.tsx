c38c61eb127a76ea8125572ce0aea7293720d489
Cyril Ghali <contact@cyrilghali.fr>
2024-04-03 20:01:13 +0200

    bump to next 14 and fix images

--- components/DonationCTA.tsx
+++ components/DonationCTA.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { HomeTitle } from './Title'
 export default function DonationCTA() {
   return (
@@ -17,30 +16,6 @@ export default function DonationCTA() {
           </div>
           </div>
         </div>
-      </div>
-    </div>
-  )
-}
-
-/* This example requires Tailwind CSS v2.0+ */
-export function DonationCTA2() {
-  return (
-    <div className="bg-rose-700">
-      <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-        <Image src="/assets/cross.svg" height={60} width={60} alt=""/>
-        <h2 className="text-3xl font-extrabold text-white sm:text-4xl mt-4">
-          <span className="block">Vuoi aiutare?</span>
-        </h2>
-        <p className="mt-4 text-lg leading-6 text-rose-200">
-        </p>
-        <Link
-          href="/donation">
-        <div
-          className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-rose-600 bg-white hover:bg-rose-50 sm:w-auto"
-        >
-          Donare
-        </div>
-        </Link>
       </div>
     </div>
   )
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { HomeTitle } from './Title'
-import Image from 'next/image'
 import Link from 'next/link';
 
 export const links = [
@@ -21,22 +20,18 @@ export const SocialLinks = () => {
   return (
     <div className="bg-rose-50">
         <HomeTitle title="Rimanere in contatto" />
-        <div className="flex justify-between items-center flex-col max-w-7xl mx-auto py-12 px-4 sm:px-6">
           <ul
             role="list"
-            className="mx-auto sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-2 lg:max-w-5xl"
+            className="flex justify-center"
           >
             {links.map((social) => (
-              <li key={social.alt}>
-                <div className="flex justify-center">
-                <Link href={social.href} target="_blank">
-                  <Image className="mx-auto h-40 w-40 object-contain rounded-full xl:w-56 xl:h-56 hover:cursor-pointer " width={100} height={150} src={social.imageUrl} alt="" />
+              <li key={social.alt} className="m-6">
+                <Link href={social.href} target="_blank" className="static">
+                  <img className="object-contain rounded-full hover:cursor-pointer" width={100} height={100} src={social.imageUrl} alt={social.alt} />
                 </Link>
-                </div>
               </li>
             ))}
           </ul>
-        </div>
     </div>
   )
 }
