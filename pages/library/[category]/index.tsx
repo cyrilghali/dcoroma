ef686d70370b0ccfeed13c4ba4cdbd3462745744
Cyril Ghali <contact@cyrilghali.fr>
2024-04-06 21:53:43 +0200

    dynamic folder for library

---
+++ pages/library/[category]/index.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+import { Title } from 'components/Title'
+import LibraryGrid from '@/components/LibraryGrid'
+import SearchBar from '@/components/SearchBar'
+
+function capitalize(str) {
+  if (!str) return ''
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+}
+
+async function getCategoryData(category) {
+  try {
+    const mod = await import(`data/library/${category}`)
+    return mod.default
+  } catch (err) {
+    console.error(`Could not load data for category: ${category}`, err)
+    return null // Return null to indicate failure
+  }
+}
+
+const CategoryPage = () => {
+  const router = useRouter()
+  const { category } = router.query
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    getCategoryData(category).then((data) => {
+      if (data) {
+        setItems(data)
+      } else {
+        router.replace('/404') // Programmatically navigate to the 404 page
+      }
+    })
+  }, [category, router])
+
+  const handleSearch = (searchTerm) => {
+    const filtered = items.filter((item: any) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    )
+    setItems(filtered)
+  }
+
+  if (!items) {
+    return null // or a loading indicator
+  }
+
+  return (
+    <div>
+      <Header />
+      <Title title={capitalize(category)} />
+      <SearchBar onSearch={handleSearch} />
+      <LibraryGrid docs={items} />
+      <Footer />
+    </div>
+  )
+}
+
+export default CategoryPage
