3fa5b46f8978a80bd774d544dd8e8156988c178d
Cyril Ghali <contact@cyrilghali.fr>
2024-04-08 00:28:04 +0200

    Add sanity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin={'anonymous'}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
