a544b765e25848b78de2c74b855c0f77723c5d31
Cyril Ghali <contact@cyrilghali.fr>
2024-04-07 09:40:32 +0200

    add umami script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,11 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap'
           rel='stylesheet'
         />
+        <script
+          defer
+          src='https://analytics.eu.umami.is/script.js'
+          data-website-id='3144784c-ff39-4ba4-90d6-1c4e945740c2'
+        ></script>
       </Head>
       <body>
         <Main />
